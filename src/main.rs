6a242ce7011626af56feb3dae3dec1de25cdc3f1
Wojtek Porczyk <woju@invisiblethingslab.com>
2022-06-10 23:10:21 +0200

    WIP NIE DZIAŁA
    Jeden z błędów jest taki:
    
        error[E0277]: `()` is not a future
            --> src/main.rs:102:72
            |
        102  |       tokio_stream::wrappers::ReceiverStream::new(net_receiver).for_each(move |request| {
            |  _______________________________________________________________--------_^
            | |                                                               |
            | |                                                               required by a bound introduced by this call
        103  | |         match request {
        104  | |             SmoltcpRequest::Connect(inet2vpn_receiver) => {
        105  | |                 dbg!(inet2vpn_receiver);
        ...    |
        108  | |         }
        109  | |     });
            | |_____^ `()` is not a future
            |
            = help: the trait `futures::Future` is not implemented for `()`
            = note: () must be a future or must implement `IntoFuture` to be awaited
        note: required by a bound in `futures::StreamExt::for_each`
            --> /home/user/.cargo/registry/src/github.com-1ecc6299db9ec823/futures-util-0.3.21/src/stream/stream/mod.rs:1081:14
            |
        1081 |         Fut: Future<Output = ()>,
            |              ^^^^^^^^^^^^^^^^^^^ required by this bound in `futures::StreamExt::for_each`

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,12 @@ use std::os::unix::io::AsRawFd;
 use std::sync::Mutex;
 use std::rc::Rc;
 
+
 use actix_web;
 use actix_web::{web, get, post, delete};
 
 use anyhow;
+
 
 use smoltcp;
 use smoltcp::iface;
@@ -24,7 +26,9 @@ use smoltcp::wire;
 
 use tokio;
 use tokio::io::Interest;
-use tokio::sync::mpsc;
+//use tokio::sync::mpsc;
+use futures::StreamExt;
+use futures::channel::mpsc;
 
 //use tokio_smoltcp::device;
 
@@ -37,12 +41,91 @@ struct Connection {
 
 struct ConnectionManager {
     connections: std::collections::HashMap::<u32, Connection>,
-    prev_id: u32,
+}
+
+/*
+impl ConnectionManager {
+    fn new(net: ya_relay_stack::Network) -> ConnectionManager {
+        ConnectionManager {
+            connections: std::collections::HashMap::<u32, Connection>::new(),
+            prev_id: 0,
+            net: net,
+        }
+    }
+
+    async fn delete(&mut self, id: u32) {
+        todo!()
+    }
+}
+*/
+
+/*
+ * VPN adapter
+ */
+
+
+//async fn sender(&iface) {
+//  let tcp_handle = iface.add_socket(socket::TcpSocket::new(
+//      socket::TcpSocketBuffer::new(vec![0; 64]),
+//      socket::TcpSocketBuffer::new(vec![0; 128]),
+//  ));
+
+//  let (socket, cx) = iface.get_socket_and_context::<tcp::Socket>(tcp_handle);
+//  socket.connect(cx, (10.5.6.254, 6789), 45678).unwrap();
+//  let mut tcp_is_active = false;
+//}
+
+
+#[derive(Debug)]
+enum SmoltcpRequest {
+    Connect(mpsc::Receiver<Vec<u8>>), // (remote address+port, some id)
+    Disconnect, // (id)
+}
+
+async fn network_thread(net_receiver: mpsc::Receiver<SmoltcpRequest>) {
+    let hwaddr = wire::HardwareAddress::Ethernet(
+        wire::EthernetAddress([0xb0, 0xa5, 0xa5, 0xa5, 0xa5, 0xa5]));
+    let iface = ya_relay_stack::interface::tap_iface(hwaddr, 1280);
+
+    let netconfig = Rc::new(ya_relay_stack::NetworkConfig {
+        max_transmission_unit: 1280,
+        buffer_size_multiplier: 32,
+    });
+
+    let net = ya_relay_stack::Network::new(
+        "proxy",
+        netconfig.clone(),
+        ya_relay_stack::Stack::new(iface, netconfig));
+
+    net.spawn_local();
+
+    tokio_stream::wrappers::ReceiverStream::new(net_receiver).for_each(move |request| {
+        match request {
+            SmoltcpRequest::Connect(inet2vpn_receiver) => {
+                dbg!(inet2vpn_receiver);
+            },
+            SmoltcpRequest::Disconnect => todo!(),
+        }
+    });
+}
+
+
+async fn acceptor(listener: tokio::net::TcpListener, net_sender: mpsc::Sender<SmoltcpRequest>) {
+//  let channel: ya_relay_stack::Channel::<Vec<u8>> = Default::default();
+    loop {
+        let (stream, _) = listener.accept().await.unwrap();
+        dbg!("accepted"); //TODO metrics
+        tokio::spawn(forwarder(stream, net_sender.clone())); //, channel.tx));
+    }
 }
 
 // TODO jeszcze argument dokąd to będzie forwardować
-async fn forwarder(stream: tokio::net::TcpStream) {
-    // TODO connect to remote vpn endpoint
+async fn forwarder(stream: tokio::net::TcpStream, mut net_sender: mpsc::Sender<SmoltcpRequest>) {
+
+    // TODO kanał w drugą stronę...
+    let (inet2vpn_sender, inet2vpn_receiver) = mpsc::channel::<Vec<u8>>(10);
+    net_sender.try_send(SmoltcpRequest::Connect(inet2vpn_receiver)).expect("failed to ::Connect()");
+
     loop {
         let ready = stream.ready(Interest::READABLE).await.unwrap(); // TODO | Interest::WRITABLE
         if ready.is_readable() {
@@ -64,73 +147,41 @@ async fn forwarder(stream: tokio::net::TcpStream) {
             }
         }
     }
+//  inet2vpn_sender.disconnect(); // albo close()?
 }
-
-impl ConnectionManager {
-    fn new() -> ConnectionManager {
-        ConnectionManager {
-            connections: std::collections::HashMap::<u32, Connection>::new(),
-            prev_id: 0,
-        }
-    }
-
-    async fn create(&mut self) -> u32 {
-        let listener = tokio::net::TcpListener::bind("0.0.0.0:4567").await.unwrap();
-        let acceptor = tokio::spawn(Self::acceptor(listener));
-//      let conn = Connection {
-//          acceptor: acceptor,
-//          forwarders: Vec::new(),
-//      };
-        self.prev_id += 1;
-        let id = self.prev_id;
-//      self.connections.insert(id, conn);
-        id
-    }
-
-    async fn acceptor(listener: tokio::net::TcpListener) {
-        loop {
-            let (stream, _) = listener.accept().await.unwrap();
-            dbg!("accepted"); //TODO metrics
-            tokio::spawn(forwarder(stream));
-        }
-    }
-
-    async fn delete(&mut self, id: u32) {
-        todo!()
-    }
-}
-
-/*
- * VPN adapter
- */
-
-
-//async fn sender(&iface) {
-//  let tcp_handle = iface.add_socket(socket::TcpSocket::new(
-//      socket::TcpSocketBuffer::new(vec![0; 64]),
-//      socket::TcpSocketBuffer::new(vec![0; 128]),
-//  ));
-
-//  let (socket, cx) = iface.get_socket_and_context::<tcp::Socket>(tcp_handle);
-//  socket.connect(cx, (10.5.6.254, 6789), 45678).unwrap();
-//  let mut tcp_is_active = false;
-//}
 
 
 /*
  * control API
  */
 
-#[post("/tcp")]
-async fn tcp_post(data: web::Data<Mutex<ConnectionManager>>) -> String {
-    let mut cm = data.lock().unwrap();
-    format!("id {}", cm.create().await)
+struct WebData {
+    net_sender: mpsc::Sender<SmoltcpRequest>,
+    prev_id: u32,
 }
 
+#[post("/tcp")]
+async fn tcp_post(data: web::Data<Mutex<WebData>>) -> String {
+    let mut data = data.lock().unwrap();
+    data.prev_id += 1;
+    let id = data.prev_id;
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:4567").await.unwrap();
+    let acceptor = tokio::spawn(acceptor(listener, data.net_sender.clone()));
+    format!("id {}", id)
+}
+
+/*
+    async fn create(&mut self) -> u32 {
+//      self.connections.insert(id, conn);
+        id
+    }
+*/
+
 #[delete("/tcp")]
-async fn tcp_delete(data: web::Data<Mutex<ConnectionManager>>) -> String {
-    let cm = data.lock().unwrap();
-    format!("Hello, world! {}", cm.prev_id)
+async fn tcp_delete(data: web::Data<Mutex<WebData>>) -> String {
+    let data = data.lock().unwrap();
+    format!("Hello, world! {}", data.prev_id)
 }
 
 #[get("/metrics")]
@@ -167,25 +218,10 @@ async fn metrics(data: web::Data<Mutex<ConnectionManager>>) -> String {
 
 #[actix_web::main]
 async fn main() {
-//  let (mpsc_tx, mpsc_rx) = mpsc::channel(100);
-//  let jh_iface = tokio::spawn(interface_handler(mpsc_rx));
+    let (net_sender, net_receiver) = mpsc::channel(100);
+    let jh_smoltcp = tokio::task::spawn_local(network_thread(net_receiver));
 
-    let hwaddr = wire::HardwareAddress::Ethernet(wire::EthernetAddress([0xb0, 0xa5, 0xa5, 0xa5, 0xa5, 0xa5]));
-    let iface = ya_relay_stack::interface::tap_iface(hwaddr, 1280);
-
-    let netconfig = Rc::new(ya_relay_stack::NetworkConfig {
-        max_transmission_unit: 1280,
-        buffer_size_multiplier: 32,
-    });
-
-    let net = ya_relay_stack::Network::new(
-        "proxy",
-        netconfig.clone(),
-        ya_relay_stack::Stack::new(iface, netconfig));
-
-    net.spawn_local();
-
-    let data = web::Data::new(Mutex::new(ConnectionManager::new()));
+    let data = web::Data::new(Mutex::new(WebData { net_sender: net_sender, prev_id: 0 }));
 
     actix_web::HttpServer::new(move || {
         actix_web::App::new()
